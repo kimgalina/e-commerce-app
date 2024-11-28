b9310ef3e8ccbe806965dd7fcfb38141cb076077
kimgalina <genrietta.kim@gmail.com>
2024-11-28 21:21:18 +0600

    Created entities product and category

---
+++ services/product/src/main/java/kg/kim/product/product/Product.java
@@ -0,0 +1,36 @@
+package kg.kim.product.product;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kg.kim.product.Category.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
